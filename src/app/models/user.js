db6afbaa59d8c93ba9453386277e4d20a33116c6
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-03-01 22:54:47 -0300

    Adicionado senha criptografada.
    Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: João Pedro Soares <jpcirqueira81@gmail.com>

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,10 +1,12 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
     static init(sequelize) {
         super.init(
             {
                 nome: Sequelize.STRING,
+                senha: Sequelize.VIRTUAL,
                 senha_hash: Sequelize.STRING,
                 email: Sequelize.STRING,
                 telefone: Sequelize.STRING,
@@ -16,6 +18,14 @@ class User extends Model {
             }
             
         );
+         
+        this.addHook('beforeSave', async (user) => {
+            if(user.senha){
+                user.senha_hash = await bcrypt.hash(user.senha, 8);
+            }
+        });
+
+        return this;
     }
 }
 
